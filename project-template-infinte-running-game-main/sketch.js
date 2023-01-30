0424ad7afd8fb35abac239d094307fe3fbe99918
juca <114536066+jucaown@users.noreply.github.com>
2023-01-30 18:06:17 -0300

    Add files via upload

--- project-template-infinte-running-game-main/sketch.js
+++ project-template-infinte-running-game-main/sketch.js
@@ -1,5 +1,5 @@
 var character, backgroundimg, walking_animation;
-var groundimg, ground;
+var groundimg, ground, lado1, lado2, lado3, lado4;
 
 function preload()
 {
@@ -12,6 +12,12 @@ function preload()
 function setup()
 {
     createCanvas(windowWidth,windowHeight);
+    //edges deitadas height*0.95 | height*0.05 0
+    lado1=createSprite(width*0.01, height*0.01, width, height*0.01); //x,y,w,h
+    lado2=createSprite(width*0.01, height*0.99, width, height*0.01);
+    
+    lado3=createSprite(width*0, height*0, width*0.01, height);
+    lado4=createSprite(width,   height,   width*0.01, height);
     
     background=createSprite(width/2, height/3);
     background.addImage(backgroundimg);
@@ -24,29 +30,52 @@ function setup()
     ground=createSprite(width/2,height/1.1);
     ground.addImage(groundimg);
     ground.velocityX = -5;
-    ground.scale = 0.2;
+    ground.scale = 0.2  ;
 }
 
 function draw()
 {
+
+
+character.collide(ground);
+character.collide(lado1);
+character.collide(lado2);
+character.collide(lado3);
+character.collide(lado4);
+
+
    if (keyDown("a"))
    {
-character.x = character.x -5;
+   character.x = character.x -19;
    }
 
    if (keyDown("d"))
    {
-character.x = character.x +5;
+   character.x = character.x +19;
    }
 
+  
 
-   if(ground.x < 0 )
+   if(ground.x < width/2.65)
    {
-ground.x = width/3;
+   ground.x = width/2;
+   }
+   character.velocityY = 40;
+   
+   //character.y>=height*0.59
+
+   
+
+   if (keyDown("w") && character.y>=height*0.67)
+   {
+      character.velocityY = -30;
    }
 
+   
+
+   character.velocityY = character.velocityY + 0.00001;
+   console.log(character.y)
 
 drawSprites();
 
 }
-
